3b59b8b391c5d9b4f1518dd93aec41778d2f18c6
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2016-07-13 14:52:26 +0100

    Fix broken test

--- src/test/java/manchester/synbiochem/datacapture/TestArchiverTask.java
+++ src/test/java/manchester/synbiochem/datacapture/TestArchiverTask.java
@@ -11,19 +11,19 @@ public class TestArchiverTask {
 	@Test
 	public void testResolve() {
 		URI base = URI.create("http://seek.org/example/");
-		assertEquals("http://seek.org/example/abc",
+		assertEquals(URI.create("http://seek.org/example/abc"),
 				ArchiverTask.resolveToURI(base, "abc"));
-		assertEquals("http://seek.org/example/abc/def",
+		assertEquals(URI.create("http://seek.org/example/abc/def"),
 				ArchiverTask.resolveToURI(base, "abc/def"));
-		assertEquals("http://seek.org/example/abc.def",
+		assertEquals(URI.create("http://seek.org/example/abc.def"),
 				ArchiverTask.resolveToURI(base, "abc.def"));
-		assertEquals("http://seek.org/example/abc%40def",
+		assertEquals(URI.create("http://seek.org/example/abc%40def"),
 				ArchiverTask.resolveToURI(base, "abc@def"));
-		assertEquals("http://seek.org/example/abc+def",
+		assertEquals(URI.create("http://seek.org/example/abc+def"),
 				ArchiverTask.resolveToURI(base, "abc def"));
-		assertEquals("http://seek.org/example/abc+def+ghi/jkl.mn",
+		assertEquals(URI.create("http://seek.org/example/abc+def+ghi/jkl.mn"),
 				ArchiverTask.resolveToURI(base, "abc def ghi/jkl.mn"));
-		assertEquals("http://seek.org/example/abc+def%E2%82%AC.ghi",
+		assertEquals(URI.create("http://seek.org/example/abc+def%E2%82%AC.ghi"),
 				ArchiverTask.resolveToURI(base, "abc def\u20ac.ghi"));
 	}
 }

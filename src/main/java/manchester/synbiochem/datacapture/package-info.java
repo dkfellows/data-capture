befe81a2e6e295bf4e40e00f4606c767d84cc225
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2016-10-04 11:55:36 +0100

    Trivial javadoc tweak.

--- src/main/java/manchester/synbiochem/datacapture/package-info.java
+++ src/main/java/manchester/synbiochem/datacapture/package-info.java
@@ -1,4 +1,8 @@
 /**
+ * The implementation of the Data Capture service. Overall, this is a web
+ * application hosted inside Apache CXF.
+ *
  * @author Donal Fellows
+ * @see Application
  */
 package manchester.synbiochem.datacapture;
